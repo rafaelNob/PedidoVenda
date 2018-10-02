c200f3f2d885dc6df1252c0bf5b3926b86f4d30f
legendarios <lendagiarios@gmail.com>
2018-10-02 11:40:48 -0300

    adicionando CDI Projeto

--- src/main/java/br/projetoestoque/controller/PesquisaProdutoBean.java
+++ src/main/java/br/projetoestoque/controller/PesquisaProdutoBean.java
@@ -1,14 +1,36 @@
 package br.projetoestoque.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Named;
 
-@ManagedBean
-public class PesquisaProdutoBean {
+
+
+
+@Named
+@ConversationScoped
+public class PesquisaProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	private List<Integer> produtosFiltrados = new ArrayList<>();
+	private String recebeValor;
 	
+	public String getRecebeValor() {
+		System.out.println(recebeValor);
+		return recebeValor;
+	}
+
+	public void setRecebeValor(String recebeValor) {
+		this.recebeValor = recebeValor;
+	}
+
+	public void setProdutosFiltrados(List<Integer> produtosFiltrados) {
+		this.produtosFiltrados = produtosFiltrados;
+	}
+
 	public PesquisaProdutoBean() {
 		for (int i = 0; i < 50; i++) {
 			produtosFiltrados.add(i);
